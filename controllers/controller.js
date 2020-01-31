c4f2bbb77d04c4db20f399cf0d17ab042f8002cb
Adam m <adam@floorsmtihllc.com>
2020-01-30 20:54:39 -0500

    update 2

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,26 +3,30 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var user = require("../models/index.js");
+var User = require("../models/index.js");
 
+function controller () {
+    router.get("/", function(req, res) {
+        User.all(function(data) {
+          var hbsObject = {
+            users: data
+          };
+          console.log(hbsObject);
+          res.render("index", hbsObject);
+        });
+      });
+      
+      router.post("/api/anis", function(req, res) {
+        User.create([
+          "username", "password"
+        ], [
+          req.body.username, req.body.password
+        ], function(result) {
+          // Send back the ID of the new quote
+          res.json({ id: result.insertId });
+        });
+      });
+}
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  user.all(function(data) {
-    var hbsObject = {
-      users: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
-});
 
-router.post("/api/anis", function(req, res) {
-  user.create([
-    "name", "description"
-  ], [
-    req.body.name, req.body.description
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
-});
+module.exports = controller;
