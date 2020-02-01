df1dcdfdb8ea22bab6b1b4775831ef8ba579b424
jwescott <jwescott@mail.usf.edu>
2020-01-31 22:02:06 -0500

    got the homepage to load, fixed error in index.handlebars

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,13 +1,13 @@
-var express = require("express");
 
-var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var User = require("../models/index.js");
+var db = require("../models/index.js");
 
-function controller () {
-    router.get("/", function(req, res) {
-        User.all(function(data) {
+function controller (app) {
+    app.get("/", function(req, res) {
+      console.log("Homepage");
+      
+        db.User.findAll({}).then(function(data) {
           var hbsObject = {
             users: data
           };
@@ -16,8 +16,8 @@ function controller () {
         });
       });
       
-      router.post("/api/anis", function(req, res) {
-        User.create([
+      app.post("/api/anis", function(req, res) {
+        db.User.create([
           "username", "password"
         ], [
           req.body.username, req.body.password
